a85f049befde98e13dd351a9294cb545458dab45
Julian Wyatt <Julian_Wyatt@hotmail.com>
2019-01-10 13:49:00 +0000

    removed more variables from simulation setup

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let attractor
 
 function setup(){      
            
-    attractor =  new Simulation(10,0.95,800,0.5);                                  // eslint-disable-line no-unused-vars
+    attractor =  new Simulation(10,0.95,600);                                  // eslint-disable-line no-unused-vars
     attractor.setup();
 
 }
