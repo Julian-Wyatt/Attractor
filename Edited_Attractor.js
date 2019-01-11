ad0f70dbcbda647965fc1e69de29569f57b05f7d
Julian Wyatt <Julian_Wyatt@hotmail.com>
2019-01-11 19:54:31 +0000

    continued fixing rules and bugs created by adding rules

--- Edited_Attractor.js
+++ Edited_Attractor.js
@@ -6,8 +6,10 @@
 /* eslint max-params:0*/
 /* eslint no-mixed-operators:0*/
 /* eslint max-lines:0*/
-/* eslint no-magic-numbers: ["error", { "ignore": [0,1,2,5,10,50] }]*/
+/* eslint no-magic-numbers: ["error", { "ignore": [0, 0.4, 1,2,3,4,5,6,7,8,9,10,11,12,13, 14,50,100,200,250,350,450, 500, 600 ,1000] }]*/
 /* eslint max-lines-per-function:0*/
+/* eslint no-plusplus:0*/
+/* eslint id-length:0*/
 
 /*
  * Particle class in order to define attributes for each unqiue particle
@@ -29,17 +31,19 @@ class Particle {
         this.red = red;
         this.green = green;
         this.blue = blue;
-        this.color();
+
         this.flip = Math.round(Math.random() * 2) * 2 - 1;
 
-        this.maxRandLifeVal = 30;
+        this.maxRandLifeVal = 20;
         this.minLifeVal = 10;
-        this.maxLife = random() * this.maxLifeVal + this.minLifeVal;
+        this.maxLife = random(this.maxRandLifeVal) + this.minLifeVal;
         this.currLife = this.maxLife;
-        this.mainHeight = this.height - 200;
-        this.mainWidth = this.width + 100;
+        this.mainHeight = windowHeight - 200;
+        this.mainWidth = windowWidth + 100;
         this.maxColour = 255;
         this.alpha = 32;
+        this.colorParticle();
+
 
     }
 
@@ -47,28 +51,18 @@ class Particle {
 
         this.currLife -= 0.05;
         //console.log("yposition death"+Math.round(this.yPos));
-        if (this.currLife <= 0 || this.xPos > windowWidth || this.xPos < 0 || this.yPos > this.mainHeight || this.yPos < 0) {
+        if (this.currLife <= 0 || this.xPos > windowWidth || this.xPos < 0 || this.yPos + 10 > this.mainHeight || this.yPos < 0) {
 
-           // console.log("xposition: "+this.xPos)
-            //console.log("yposition:"+this.yPos)
-            if (this.yPos > this.mainHeight) {
 
-                console.log("yposition death"+Math.round(this.yPos));
-
-            }
             this.respawn();
 
         }
-        if (this.yPos > this.mainHeight) {
 
-            console.log("yposition"+Math.round(this.yPos));
-
-        }
 
     }
 
 
-    color () {
+    colorParticle () {
 
         let speed = dist(0, 0, this.xSpeed, this.ySpeed) * this.maxColour % this.maxColour;
 
@@ -82,9 +76,9 @@ class Particle {
 
     respawn () {
 
-        this.currLife = this.maxLife;
-        this.xPos = random() * this.mainWidth;
-        this.yPos = random() * this.mainHeight;
+        this.currLife = random(this.maxLifeVal) + this.minLifeVal;
+        this.xPos = random(this.mainWidth);
+        this.yPos = random(this.mainHeight);
 
     }
 
@@ -116,15 +110,19 @@ class Simulation {
 
 
         this.clearButton = createButton("Clear");
-        this.clearButton.position(this.mainWidthForSliders - 500, this.mainHeightForSliders);
+        this.clearButton.position(this.mainWidthForSliders - 600, this.mainHeightForSliders);
         this.clearButton.mousePressed(this.clearButtonFunc);
 
+        this.blendButton = createButton("Change Blend Mode");
+        this.blendButton.position(this.mainWidthForSliders - 525, this.mainHeightForSliders);
+        this.blendButton.mousePressed(this.changeBlendMode);
+
         this.seedButton = createButton("Randomise Seed");
-        this.seedButton.position(this.mainWidthForSliders - 450, this.mainHeightForSliders);
+        this.seedButton.position(this.mainWidthForSliders - 400, this.mainHeightForSliders);
         this.seedButton.mousePressed(this.seedButtonFunc);
 
         this.rateSlider = createSlider(this.minRate, this.maxRate, this.mainRate, 0);
-        this.rateSlider.position(this.mainWidthForSliders - 250, tthis.mainHeightForSliders);
+        this.rateSlider.position(this.mainWidthForSliders - 250, this.mainHeightForSliders);
 
         this.rSlider = createSlider(0, this.maxColour, this.maxColour);
         this.rSlider.position(this.mainWidthForSliders - 50, this.mainHeightForSliders - 50);
@@ -143,141 +141,122 @@ class Simulation {
         this.radiusSlider.position(this.mainWidthForSliders + 250, this.mainHeightForSliders);
         this.radiusSlider.style("width", "50px");
 
-        this.randomiseColourCheckbox = createCheckbox("Randomise colour on click", false)
-        this.randomiseColourCheckbox.position(this.width/2 +350, this.height -100) 
-        //this.randomiseColourCheckbox.changed(this.randomCheckEvent);
-
-
-
-
-
+        this.randomiseColourCheckbox = createCheckbox("Randomise colour on click", false);
+        this.randomiseColourCheckbox.position(this.mainWidthForSliders + 350, this.mainHeightForSliders);
+        this.randomiseColourCheckbox.changed(this.randomCheckEvent);
 
 
         this.magnetism = magnetism;
         this.deceleration = deceleration;
-        
-        
-        /*
-        this.xAccn = new Array(this.total);
-        this.xPos = new Array(this.total);
-        this.xSpeed = new Array(this.total);
-        this.yAccn = new Array(this.total);
-        this.yPos = new Array(this.total);
-        this.ySpeed = new Array(this.total);
-        */
+
         this.particles = new Array(this.total);
         this.noiseScale = noiseScale;
-        
+
         this.mouseX = mouseX;
         this.mouseY = mouseY;
-        
-        
 
-        
+        this.blendChange = 0;
 
-
+        this.setup();
 
 
-        for (let i=0;i<this.total;i++){
-            
-            this.particles[i] = new Particle(Math.round(Math.random()*(this.width+100)),Math.round(Math.random()*(this.height-200)),this.radiusSlider.value(),0,0);
+        for (let particle = 0; particle < 100; particle += 1) {
+
+            this.particles[particle] = new Particle(Math.round(Math.random() * this.width), Math.round(Math.random() * (this.mainHeightForSliders - 100)), this.radiusSlider.value(), 0, 0, 0, 0, this.rSlider.value(), this.gSlider.value(), this.gSlider.value());
 
 
         }
-        console.log(this.height+"\n"+(this.height-200));
+
     }
 
 
 
 
-    setup() {
+    setup () {
 
-       
-        createCanvas(this.width,this.height);
-        noStroke(); 
+
+        createCanvas(this.width, this.height);
+        noStroke();
         fill(0);
         ellipseMode(RADIUS);
         background(0);
-        //white rectangle for buttons and sliders
-        fill(255)
-        rect(0,this.height-200,this.width,200);
-        //can use switch case to change blend mode on button press
-        blendMode(BLEND);   
+        // White rectangle for buttons and sliders
+        fill(this.maxColour);
+        rect(0, this.mainHeightForSliders - 100, this.width, 200);
 
-        noiseSeed(random()*this.noiseScale*1000*random());
-       // this.sliders();
+        // Can use switch case to change blend mode on button press
+        blendMode(BLEND);
+
+        noiseSeed(random() * this.noiseScale * 1000 * random());
+
     }
 
 
+    draw () {
 
-    draw(){
 
+        for (let i = 0; i < this.total; i++) {
 
-        //fill(0,0,0);
-        
-  
-        for(let i=0; i<this.total; i++){
-            let distance = dist(this.mouseX, this.mouseY, this.particles[i].xPos, this.particles[i].yPos); 
+            const distance = dist(this.mouseX, this.mouseY, this.particles[i].xPos, this.particles[i].yPos);
 
-            if(distance > 3){
-                this.particles[i].xAccn = this.magnetism * (this.mouseX - this.particles[i].xPos) / (distance * distance); 
+            if (distance > 3) {
+
+                this.particles[i].xAccn = this.magnetism * (this.mouseX - this.particles[i].xPos) / (distance * distance);
                 this.particles[i].yAccn = this.magnetism * (this.mouseY - this.particles[i].yPos) / (distance * distance);
+
             }
-            this.particles[i].xSpeed += this.particles[i].xAccn; 
-            this.particles[i].ySpeed += this.particles[i].yAccn; 
-    
+            this.particles[i].xSpeed += this.particles[i].xAccn;
+            this.particles[i].ySpeed += this.particles[i].yAccn;
+
             this.particles[i].xSpeed *= this.deceleration;
             this.particles[i].ySpeed *= this.deceleration;
-    
-            this.particles[i].xPos += this.particles[i].xSpeed; 
-            this.particles[i].yPos += this.particles[i].ySpeed; 
 
-            if (!this.randomiseColourCheckbox.checked()){
+            this.particles[i].xPos += this.particles[i].xSpeed;
+            this.particles[i].yPos += this.particles[i].ySpeed;
+
+            if (!this.randomiseColourCheckbox.checked()) {
 
                 this.particles[i].r = this.rSlider.value();
                 this.particles[i].g = this.gSlider.value();
                 this.particles[i].b = this.bSlider.value();
+
             }
-            this.particles[i].color();
+            this.particles[i].colorParticle();
+
         }
-    //let particle = new Particle(100,100,100,100);
+
+
     }
 
 
-    perlinNoise(){
-        
+    perlinNoise () {
 
-        
-        for (let i=0;i<this.total;i++){
-            let angle = noise(this.particles[i].xPos/this.noiseScale,this.particles[i].yPos/this.noiseScale)*2*Math.PI*this.noiseScale*this.particles[i].flip;
-            //let temp = this.particles[i].xSpeed;
-            //let temp1 = this.particles[i].ySpeed;
-            this.particles[i].ySpeed = lerp(this.particles[i].ySpeed,Math.sin(angle)*this.rateSlider.value(),0.4);
-            this.particles[i].xSpeed = lerp(this.particles[i].xSpeed,Math.cos(angle)*this.rateSlider.value(),0.4);
-            //this.particles[i].xSpeed = Math.cos(angle)*this.rate;
-            
-            
-            //this.particles[i].xPos += lerp(temp,this.particles[i].xSpeed,0.05); 
-            //this.particles[i].yPos += lerp(temp1,this.particles[i].ySpeed,0.05); 
-            this.particles[i].xPos+=this.particles[i].xSpeed;
-            this.particles[i].yPos+=this.particles[i].ySpeed;
 
-            if (!this.randomiseColourCheckbox.checked()){
+        for (let i = 0; i < this.total; i++) {
+
+            const angle = noise(this.particles[i].xPos / this.noiseScale, this.particles[i].yPos / this.noiseScale) * 2 * Math.PI * this.noiseScale * this.particles[i].flip;
+
+            this.particles[i].ySpeed = lerp(this.particles[i].ySpeed, Math.sin(angle) * this.rateSlider.value(), 0.4);
+            this.particles[i].xSpeed = lerp(this.particles[i].xSpeed, Math.cos(angle) * this.rateSlider.value(), 0.4);
+
+            this.particles[i].xPos += this.particles[i].xSpeed;
+            this.particles[i].yPos += this.particles[i].ySpeed;
+
+            if (!this.randomiseColourCheckbox.checked()) {
+
 
                 this.particles[i].r = this.rSlider.value();
                 this.particles[i].g = this.gSlider.value();
                 this.particles[i].b = this.bSlider.value();
+
             }
-            this.particles[i].color();
-            
+            this.particles[i].colorParticle();
+
             this.particles[i].checkDeath();
 
             smooth();
 
         }
-
-    
-
 
 
     }
@@ -285,66 +264,66 @@ class Simulation {
 
     run () {
 
-        //black rectangle which also gets trail to fade
-        fill(0,0,0,5)
-        rect(0,0,this.width,this.height-200)
-      
-            if (mouseIsPressed==false) {
-
-                this.perlinNoise();
-
-                this.runOnce=false;
-            }
-            else {
-                
-                if (mouseY<this.height-200) {
-
-                this.mouseX = mouseX;
-                this.mouseY = mouseY;
-                
-                if (this.randomiseColourCheckbox.checked() && this.runOnce ==false){
-                   
-                    for ( let i =0; i<this.total; i++ ){
-                        this.particles[i].r = Math.round(random(255));
-                        this.particles[i].g = Math.round(random(255));
-                        this.particles[i].b = Math.round(random(255));
-                        this.particles[i].color();
-                        console.log('Checking!');
-                    }
-                    this.runOnce = true;   
-                    
-                }
-                this.draw();
-                }
-                else{
-                    this.perlinNoise();
-                }
-        
-            }
-
-
         this.updateTotalParticles();
         this.updatePartcileRadius();
+        // Black rectangle which also gets trail to fade
 
-        //console.log(this.total);
-    }
+        fill(0, 0, 0, 5);
+        rect(0, 0, this.width, this.height - 200);
 
-   
+        if (mouseIsPressed === false) {
 
-    clearButtonFunc(){
+            this.perlinNoise();
 
-        fill (0,0,0);
-        rect(0,0,windowWidth,windowHeight-200)
-        for(let i=0; i<this.total; i++){
-            this.particles[i].respawn();
+            this.runOnce = false;
+
+        } else if (mouseY < this.height - 200) {
+
+            this.mouseX = mouseX;
+            this.mouseY = mouseY;
+
+            if (this.randomiseColourCheckbox.checked() && this.runOnce === false) {
+                   
+                for (let i = 0; i < this.total; i++) {
+
+                    this.particles[i].r = Math.round(random(this.maxColour));
+                    this.particles[i].g = Math.round(random(this.maxColour));
+                    this.particles[i].b = Math.round(random(this.maxColour));
+                    this.particles[i].colorParticle();
+                    console.log('Checking!');
+
+                }
+                this.runOnce = true;
+
+            }
+            this.draw();
+
+        } else {
+
+            this.perlinNoise();
+
         }
 
     }
 
-    seedButtonFunc(){
-        fill (0,0,0);
-        rect(0,0,windowWidth,windowHeight-200)
-        noiseSeed(random()* this.noiseScale *1000*random());
+    clearButtonFunc () {
+
+        fill(0, 0, 0);
+        rect(0, 0, windowWidth, windowHeight - 200);
+        for (let i = 0; i < this.total; i++) {
+
+            this.particles[i].respawn();
+
+        }
+
+    }
+
+    seedButtonFunc () {
+
+        fill(0, 0, 0);
+        rect(0, 0, windowWidth, windowHeight - 200);
+        noiseSeed(random() * this.noiseScale * 1000 * random());
+
     }
 
     updateTotalParticles(){
@@ -386,7 +365,7 @@ class Simulation {
                     this.particles[i].r = Math.round(random(255));
                     this.particles[i].g = Math.round(random(255));
                     this.particles[i].b = Math.round(random(255));
-                    this.particles[i].color();
+                    this.particles[i].colorParticle();
                     console.log('Checking!');
                 }
                 this.runOnce = true;
@@ -397,11 +376,59 @@ class Simulation {
             console.log('Unchecking!');
           }
     }
-    
 
-   
+    changeBlendMode () {
+
+        this.blendChange++;
+
+        switch (this.blendChange % 14) {
+
+        default:
+            blendMode(BLEND);
+            break;
+        case 1:
+            blendMode(ADD);
+            break;
+        case 2:
+            blendMode(DARKEST);
+            break;
+        case 3:
+            blendMode(LIGHTEST);
+            break;
+        case 4:
+            blendMode(DIFFERENCE);
+            break;
+        case 5:
+            blendMode(EXCLUSION);
+            break;
+        case 6:
+            blendMode(MULTIPLY);
+            break;
+        case 7:
+            blendMode(SCREEN);
+            break;
+        case 8:
+            blendMode(REPLACE);
+            break;
+        case 9:
+            blendMode(OVERLAY);
+            break;
+        case 10:
+            blendMode(HARD_LIGHT);
+            break;
+        case 11:
+            blendMode(SOFT_LIGHT);
+            break;
+        case 12:
+            blendMode(DODGE);
+            break;
+        case 13:
+            blendMode(BURN);
+            break;
+
+        }
+
+    }
 
 }
-
-
 
