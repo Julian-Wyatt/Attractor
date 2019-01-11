ab9cd908cd7d548cabb0599b6bce4d9d5a2aa8e3
Julian Wyatt <Julian_Wyatt@hotmail.com>
2019-01-11 10:40:27 +0000

    fixed some eslint rules

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 let attractor
 
-function setup(){      
+function setup() {      // eslint-disable-line no-unused-vars
            
-    attractor =  new Simulation(10,0.95,600);                                  // eslint-disable-line no-unused-vars
+    attractor =  new Simulation(10, 0.95, 600);                                  // eslint-disable-line no-undef
     attractor.setup();
 
 }
-function draw(){                                                // eslint-disable-line no-unused-vars
+function draw() {                                                // eslint-disable-line no-unused-vars
 
     attractor.run();
      
