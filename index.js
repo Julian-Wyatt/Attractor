ee58c7d840f431d3962525fcc3052497897d0f27
Julian Wyatt <Julian_Wyatt@hotmail.com>
2019-01-14 10:24:51 +0000

    updated parameters so that it is dictionary

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-let attractor
+let attractor;
 
 function setup() {      // eslint-disable-line no-unused-vars
            
-    attractor =  new Simulation(10, 0.95, 1500);                                  // eslint-disable-line no-undef
+    attractor =  new Simulation({magnetism:10, deceleration:0.95, noiseScale:1500, total:200, radius:3, rate: 0.5});                                  // eslint-disable-line no-undef
 
 }
 function draw() {                                                // eslint-disable-line no-unused-vars
