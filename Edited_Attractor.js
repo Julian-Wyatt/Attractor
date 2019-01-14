ebfdb7901ff0c35d173fab22257f0a6acbdde41b
Julian Wyatt <Julian_Wyatt@hotmail.com>
2019-01-14 17:28:32 +0000

    added getters and setters also added html total and rate

--- Edited_Attractor.js
+++ Edited_Attractor.js
@@ -19,11 +19,10 @@
 
 class Particle {
 
-    constructor (xPos, yPos, size, xSpeed, ySpeed, xAccn, yAccn, red, green, blue) {
+    constructor ({xPos, yPos, size, xSpeed, ySpeed, xAccn, yAccn, red, green, blue}) {
 
         this.xPos = xPos;
         this.yPos = yPos;
-        this.size = size;
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
         this.xAccn = xAccn;
@@ -42,7 +41,8 @@ class Particle {
         this.mainWidth = windowWidth;
         this.maxColour = 255;
         this.alpha = 32;
-        this.colorParticle();
+
+
 
 
     }
@@ -52,7 +52,6 @@ class Particle {
         this.currLife -= 0.05;
         // Console.log("yposition death"+Math.round(this.yPos));
         if (this.currLife <= 0 || this.xPos > windowWidth || this.xPos < 0 || this.yPos + 10 > this.mainHeight-200 || this.yPos < 0) {
-
 
             this.respawn();
 
@@ -71,6 +70,7 @@ class Particle {
         fill(this.r, this.g, this.b, this.alpha);
         ellipse(this.xPos, this.yPos, this.size, this.size);
 
+
     }
 
 
@@ -82,14 +82,98 @@ class Particle {
 
     }
 
+
+
+    getXPos () {
+        return this.xPos;
+    }
+
+    setXPos (value) {
+        this.xPos = value;
+    }
+
+    getYPos () {
+        return this.yPos;
+    }
+
+    setYPos (value) {
+        this.yPos = value;
+    }
+
+    getXSpeed () {
+        return this.xSpeed;
+    }
+
+    setXSpeed (value) {
+        this.xSpeed = value;
+    }
+
+    getYSpeed () {
+        return this.ySpeed;
+    }
+
+    setYSpeed (value) {
+        this.ySpeed = value;
+    }
+
+    getXAccn () {
+        return this.xAccn;
+    }
+
+    setXAccn (value) {
+        this.xAccn = value;
+    }
+
+    getYAccn () {
+        return this.yAccn;
+    }
+
+    setYAccn (value) {
+        this.yAccn = value;
+    }
+
+    getRed() {
+        return this.r;
+    }
+
+    setRed (value) {
+        this.r = value;
+    }
+
+    getGreen () {
+        return this.g;
+    }
+
+    setGreen (value) {
+        this.g = value;
+    }
+
+    getBlue() {
+        return this.b;
+    }
+
+    setBlue (value) {
+        this.b = value;
+    }
+
+    
+
+
+
 }
+
+
+
+
+
+
 
 class Simulation {
 
     constructor ({magnetism, deceleration, noiseScale, mouseX, mouseY, renderer, total, radius, rate}) {
 
 
-
+        
 
         this.runOnce = false;
         this.drawing = false;
@@ -99,16 +183,15 @@ class Simulation {
         this.mainHeightForSliders = this.height - 100;
         this.mainWidthForSliders = this.width / 2;
 
-        this.mainRate = rate;
-        this.minRate = 0.25;
-        this.maxRate = 1.75;
+        this.rate = rate;
 
         this.minRadius = 2;
         this.maxRadius = 10;
-        this.mainRadius = radius;
+        this.radius = radius;
         this.maxColour = 255;
-        this.total = total;
 
+        this.total = total;
+        
 
         this.clearButton = createButton("Clear");
         this.clearButton.position(this.mainWidthForSliders - 600, this.mainHeightForSliders);
@@ -122,9 +205,6 @@ class Simulation {
         this.seedButton.position(this.mainWidthForSliders - 400, this.mainHeightForSliders);
         this.seedButton.mousePressed(this.seedButtonFunc);
 
-        this.rateSlider = createSlider(this.minRate, this.maxRate, this.mainRate, 0);
-        this.rateSlider.position(this.mainWidthForSliders - 250, this.mainHeightForSliders);
-
         this.rSlider = createSlider(0, this.maxColour, this.maxColour);
         this.rSlider.position(this.mainWidthForSliders - 50, this.mainHeightForSliders - 50);
 
@@ -134,13 +214,6 @@ class Simulation {
         this.bSlider = createSlider(0, this.maxColour, 0);
         this.bSlider.position(this.mainWidthForSliders - 50, this.mainHeightForSliders + 50);
 
-        this.totalParticlesSlider = createSlider(5, this.total, this.total / 10, 5);
-        this.totalParticlesSlider.position(this.mainWidthForSliders + 100, this.mainHeightForSliders);
-        this.totalParticlesSlider.style("width", "100px");
-
-        this.radiusSlider = createSlider(this.minRadius, this.maxRadius, this.mainRadius, 0);
-        this.radiusSlider.position(this.mainWidthForSliders + 250, this.mainHeightForSliders);
-        this.radiusSlider.style("width", "50px");
 
         this.randomiseColourCheckbox = createCheckbox("Randomise colour on click", false);
         this.randomiseColourCheckbox.position(this.mainWidthForSliders + 350, this.mainHeightForSliders);
@@ -161,9 +234,9 @@ class Simulation {
         this.setup();
 
 
-        for (let particle = 0; particle < 100; particle += 1) {
+        for (let particle = 0; particle < this.total; particle += 1) {
 
-            this.particles[particle] = new Particle(Math.round(Math.random() * this.width), Math.round(Math.random() * (this.mainHeightForSliders - 100)), this.radiusSlider.value(), 0, 0, 0, 0, this.rSlider.value(), this.gSlider.value(), this.gSlider.value());
+            this.particles[particle] = new Particle({xPos: Math.round(Math.random() * this.width), yPos: Math.round(Math.random() * (this.mainHeightForSliders - 100)), size: this.radius, xSpeed:0, ySpeed:0, xAccn: 0, yAccn: 0, r: this.rSlider.value(), g: this.gSlider.value(), b: this.gSlider.value()});
 
 
         }
@@ -239,8 +312,8 @@ class Simulation {
 
             const angle = noise(this.particles[i].xPos / this.noiseScale, this.particles[i].yPos / this.noiseScale) * 2 * Math.PI * this.noiseScale * this.particles[i].flip;
 
-            this.particles[i].ySpeed = lerp(this.particles[i].ySpeed, Math.sin(angle) * this.rateSlider.value(), 0.4);
-            this.particles[i].xSpeed = lerp(this.particles[i].xSpeed, Math.cos(angle) * this.rateSlider.value(), 0.4);
+            this.particles[i].ySpeed = lerp(this.particles[i].ySpeed, Math.sin(angle) * this.rate, 0.4);
+            this.particles[i].xSpeed = lerp(this.particles[i].xSpeed, Math.cos(angle) * this.rate, 0.4);
 
             this.particles[i].xPos += this.particles[i].xSpeed;
             this.particles[i].yPos += this.particles[i].ySpeed;
@@ -267,8 +340,7 @@ class Simulation {
 
     run () {
 
-        this.updateTotalParticles();
-        this.updatePartcileRadius();
+
         // Black rectangle which also gets trail to fade
 
         fill(0, 0, 0, 5);
@@ -306,7 +378,7 @@ class Simulation {
             this.perlinNoise();
 
         }
-
+        
     }
 
     clearButtonFunc () {
@@ -326,41 +398,38 @@ class Simulation {
         fill(0, 0, 0);
         rect(0, 0, windowWidth, windowHeight - 200);
         noiseSeed(random() * 10000);
-        
+
     }
 
-    updateTotalParticles () {
+    updateTotalParticles (value) {
+        value --;
+        if (this.total < value) {
 
-        if (this.total < this.totalParticlesSlider.value()) {
+            for (let i = this.total; i < value; i++) {
 
-            for (let i = this.total; i < this.totalParticlesSlider.value(); i++) {
-
-                this.particles[i] = new Particle(Math.round(Math.random() * (this.width + 100)), Math.round(Math.random() * (this.height - 200)), this.radius, 0, 0);
+                this.particles[i] = new Particle({xPos: Math.round(Math.random() * this.width), yPos: Math.round(Math.random() * (this.mainHeightForSliders - 100)), size: this.radiusSlider.value(), xSpeed:0, ySpeed:0, xAccn: 0, yAccn: 0, r: this.rSlider.value(), g: this.gSlider.value(), b: this.gSlider.value()});
 
             }
 
-            this.total = this.totalParticlesSlider.value();
+            this.total = value;
 
-        } else if (this.total > this.totalParticlesSlider.value()) {
 
-            for (let i = this.total; i > this.totalParticlesSlider.value(); i--) {
+        } else if (this.total > value) {
+
+            for (let i = this.total; i > value; i--) {
 
                 delete this.particles[i];
 
             }
-            this.total = this.totalParticlesSlider.value();
+            this.total = value;
 
         }
 
     }
 
-    updatePartcileRadius () {
+    updateParticleRadius (value) {
 
-        for (let i = 0; i < this.total; i++) {
-
-            this.particles[i].size = this.radiusSlider.value();
-
-        }
+        Particle.size = (value);
 
     }
 
@@ -445,6 +514,74 @@ class Simulation {
         }
 
     }
+
+
+
+    getTotal () {
+        return this.total;
+    }
+
+    setTotal (value) {
+        this.total = value;
+    }
+
+    getMagnetism () {
+        return this.magnetism;
+    }
+
+    setMagnetism (value) {
+        this.magnetism = value;
+    }
+
+    getDeceleration () {
+        return this.deceleration;
+    }
+
+    setDeceleration (value) {
+        this.deceleration = value;
+    }
+
+    getRadius () {
+        return this.radius;
+    }
+
+    setRadius (value) {
+        this.radius = value;
+    }
+
+    getRate () {
+        return this.rate;
+    }
+
+    setRate (value) {
+        this.rate = value;
+    }
+
+    getRed() {
+        return this.r;
+    }
+
+    setRed (value) {
+        this.r = value;
+    }
+
+    getGreen () {
+        return this.g;
+    }
+
+    setGreen (value) {
+        this.g = value;
+    }
+
+    getBlue() {
+        return this.b;
+    }
+
+    setBlue (value) {
+        this.b = value;
+    }
+
+
 
 }
 
