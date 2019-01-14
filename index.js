b0589871a90c13be9b74e4caff881d2e711696e9
Julian Wyatt <Julian_Wyatt@hotmail.com>
2019-01-14 17:28:44 +0000

    added event listeners

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ function draw() {                                                // eslint-disab
     attractor.run();
      
 }
+
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    let totalSlider = document.getElementById("totalSlider");
+    function setTotal(event){
+        let totalVal = totalSlider.value;
+        document.getElementById("totalSliderOut").textContent = (totalVal-1);
+        attractor.updateTotalParticles(totalVal);
+    }
+    totalSlider.addEventListener("input",setTotal);
+
+    let rateSlider = document.getElementById("rateSlider");
+    function setRate(event){
+        let rateVal = rateSlider.value;
+        document.getElementById("rateSliderOut").textContent = rateVal;
+        attractor.setRate(rateVal);
+    }
+    rateSlider.addEventListener("input",setRate);
+
+    let radiusSlider = document.getElementById("radiusSlider");
+    function setRadius(event){
+        let radiusVal = radiusSlider.value;
+        document.getElementById("radiusSliderOut").textContent = radiusVal;
+        attractor.updateParticleRadius(radiusVal);
+
+    }
+    radiusSlider.addEventListener("input",setRadius);
+})
